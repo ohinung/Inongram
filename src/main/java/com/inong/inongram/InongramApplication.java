796737ee0d27e7336a94a3a09deb26fe1c07ed8a
LAPTOP-9OA65T2J\오인웅 <oh114114@naver.com>
2025-06-25 19:15:37 +0900

    [오인웅] -Inongram 프로젝트 생성 -thymeleaf,mybatis,mysql 설정

---
+++ src/main/java/com/inong/inongram/InongramApplication.java
@@ -0,0 +1,13 @@
+package com.inong.inongram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InongramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InongramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inong/inongram/InongramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inong.inongram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InongramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
